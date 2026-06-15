429685f1a4e3d67c8ea33baf2f91686af6b40ae2
Yashpal Gawali <gawaliyashpal@gmail.com>
2026-06-15 17:44:49 +0530

    assetmanagement microservices created

---
+++ company/src/main/java/com/example/demo/service/ICompanyService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Company;
+
+public interface ICompanyService {
+
+	public void saveCompany(Company company);	
+	public Company getCompanyById(Long id);
+	public Company getCompanyByName(String name);
+	public List<Company> getAllCompanies();
+	public void updateCompany(Company company);
+}
